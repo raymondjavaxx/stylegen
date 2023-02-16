beba1b1af850d00840a5c41b6cc0ac9a76c66a77
Ramon Torres <raymondjavaxx@gmail.com>
2023-02-15 20:02:59 -0500

    Update tests

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -11,6 +11,20 @@ class TestCLI < MiniTest::Test
   end
 
   def test_init
+    current_dir = Dir.pwd
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        stdout, _stderr, status = Open3.capture3("#{current_dir}/bin/stylegen init")
+        assert_equal(0, status.exitstatus)
+        assert_includes stdout, "Generated 'theme.yaml'."
+
+        assert_path_exists('theme.yaml', "Expected 'theme.yaml' to exist.")
+        assert_equal File.read('theme.yaml'), File.read("#{current_dir}/lib/stylegen/cli/template.yaml")
+      end
+    end
+  end
+
+  def test_init_with_custom_output
     Dir.mktmpdir do |dir|
       stdout, _stderr, status = Open3.capture3("bin/stylegen init -o #{dir}/theme.yaml")
       assert_equal(0, status.exitstatus)
@@ -31,11 +45,19 @@ class TestCLI < MiniTest::Test
   end
 
   def test_build
+    current_dir = Dir.pwd
     Dir.mktmpdir do |dir|
-      _stdout, stderr, status = Open3.capture3("bin/stylegen build -i #{dir}/theme.yaml")
-      assert_equal(1, status.exitstatus)
-      assert_includes stderr, "'#{dir}/theme.yaml' not found. Create one with 'stylegen init'."
+      Dir.chdir(dir) do
+        File.write("#{dir}/theme.yaml", File.read("#{current_dir}/lib/stylegen/cli/template.yaml"))
+        stdout, _stderr, status = Open3.capture3("#{current_dir}/bin/stylegen build")
+        assert_equal(0, status.exitstatus)
+        assert_includes stdout, "Generated 'Colors.swift' with 5 colors."
+      end
+    end
+  end
 
+  def test_build_with_custom_input
+    Dir.mktmpdir do |dir|
       File.write("#{dir}/theme.yaml", File.read('lib/stylegen/cli/template.yaml'))
 
       stdout, _stderr, status = Open3.capture3("bin/stylegen build -i #{dir}/theme.yaml")
